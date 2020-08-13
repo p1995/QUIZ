bd3ec519c691e62c995a67aabe0d2de40a9188f9
p1995 <pj1311995@gmail.com>
2020-08-13 23:37:04 +0530

    fluid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Home from "./Components/Home";
 import "./App.css";
+import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./Redux/Store";
 function App() {
   return (
     <div className="App bg-light">
-      <Provider store={store}>
-        <Home />
-      </Provider>
+      <Container fluid>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </Container>
     </div>
   );
 }
