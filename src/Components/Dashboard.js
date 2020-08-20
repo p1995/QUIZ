4a412b88c966cdc3a1b465b1a80cb5cc87773239
p1995 <pj1311995@gmail.com>
2020-08-20 22:26:52 +0530

    Final

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -80,6 +80,146 @@ function Dashboard() {
               <p>Entertainment:Telivsion</p>
             </blockquote>
           </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=15"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Entertainment:Video Games</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=16"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Entertainment:Board Games</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=17"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Science and Nature</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=18"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Science : Computers</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=19"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Science : Mathematics</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=20"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Mythology</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=21"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Sports</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=22"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Geography</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=23"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>History</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=24"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Politics</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=24"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Politics</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=25"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Art</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=26"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Celebrities</p>
+            </blockquote>
+          </Card>
+          <Card
+            bg="primary"
+            text="white"
+            className="text-center "
+            url="https://opentdb.com/api.php?amount=10&category=27"
+          >
+            <blockquote className="blockquote mb-0 card-body">
+              <p>Animals</p>
+            </blockquote>
+          </Card>
         </CardColumns>
       )}
     </div>
